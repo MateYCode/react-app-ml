8c6b8931ad9962be4c5457e7c4b927b79642dece
MateYCode <naclonso@gmail.com>
2019-02-09 22:41:22 -0300

    Updates

---
+++ precache-manifest.e5c98928513d1bc8b666b86dd212457a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf50f308299247252a27e30899a5fbd5",
+    "url": "/react-app-ml/static/media/filledCart.bf50f308.png"
+  },
+  {
+    "revision": "792b4868934f5148c07053ba4c291f7e",
+    "url": "/react-app-ml/static/media/emptyCart.792b4868.png"
+  },
+  {
+    "revision": "4f2c16371391d71b37b2",
+    "url": "/react-app-ml/static/js/runtime~main.4f2c1637.js"
+  },
+  {
+    "revision": "bc9cf16de5c9143e3fdf",
+    "url": "/react-app-ml/static/js/main.bc9cf16d.chunk.js"
+  },
+  {
+    "revision": "d0657d8410394a263459",
+    "url": "/react-app-ml/static/js/1.d0657d84.chunk.js"
+  },
+  {
+    "revision": "bc9cf16de5c9143e3fdf",
+    "url": "/react-app-ml/static/css/main.4e194bd1.chunk.css"
+  },
+  {
+    "revision": "edeaa9d041224b05b811fe2f88be412c",
+    "url": "/react-app-ml/index.html"
+  }
+];
